148e857dfe2ee40769f13789c81d1e7ede634f60
tmdcheol <leesc08392@gmail.com>
2025-03-01 16:37:54 +0900

    feat: first commit

---
+++ src/main/java/com/gn/gn_repair/GnRepairApplication.java
@@ -0,0 +1,13 @@
+package com.gn.gn_repair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnRepairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gn/gn_repair/GnRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gn.gn_repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnRepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
